08c71446f6e8c54df3633213fb60521325f72411
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-07-25 11:40:00 +0300

    I added functionality for user login, registration and logout

--- PemoranchApp/urls.py
+++ PemoranchApp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('registration/', views.registration_view, name='registration'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- PemoranchApp/views.py
+++ PemoranchApp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import logout
 
 
 def homepage(request):
-    return render(request, 'PemoranchApp/base.html')
+    return render(request, 'PemoranchApp/homepage.html')
+
+
+
+def registration_view(request):
+    return render(request, 'PemoranchApp/registration/registration.html')
+
+
+def login_view(request):
+    return render(request, 'PemoranchApp/registration/login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('homepage')
