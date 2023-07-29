ce4979733e812199e5ec9c9f9c89a809a64ad338
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-07-29 12:27:12 +0300

    Add new errors handling in homepage.html and add some rest-framework settings

--- Pemoranch/settings.py
+++ Pemoranch/settings.py
@@ -115,3 +115,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES' : [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
