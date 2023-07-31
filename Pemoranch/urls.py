e82445f9df2b3c386a39b4b5aef7545ed711dbd8
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-07-31 15:24:44 +0300

    Refactore some url paths for more flexebility

--- Pemoranch/urls.py
+++ Pemoranch/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('pemos/', views.pemoran_list_view),
     path('pemos/<int:pemoran_id>', views.pemoran_details_view, name='pemoran_details_view'),
     path('create-pemoran', views.pemoran_create_view, name='pemoran_create_view'),
-    path('api/pemos/<int:pemoran_id>/delete', views.pemoran_delete_view, name='pemoran_delete_view'),
-    path('api/pemos/action', views.pemoran_action_view, name='pemoran_action_view'),
+    path('api/pemos/', include('pemoranchees.urls'))
     
 ]
--- pemoranchees/urls.py
+++ pemoranchees/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.pemoran_list_view),
+    path('<int:pemoran_id>/', views.pemoran_details_view, name='pemoran_details_view'),
+    path('create-pemoran/', views.pemoran_create_view, name='pemoran_create_view'),
+    path('<int:pemoran_id>/delete/', views.pemoran_delete_view, name='pemoran_delete_view'),
+    path('action/', views.pemoran_action_view, name='pemoran_action_view'),
     
 ]
+
