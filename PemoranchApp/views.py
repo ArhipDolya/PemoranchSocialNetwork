dfbb328083a0b1815febffdbafaa9992487bf4dc
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-08-05 14:18:04 +0300

    Add login functionality and styling

--- PemoranchApp/views.py
+++ PemoranchApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from django.contrib import messages
+
 from .forms import LoginForm, RegisterForm
 
 
@@ -13,11 +15,38 @@ def registration_view(request):
     if request.method == 'GET':
         form = RegisterForm()
 
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You registered successfully!')
+            login(request, user)
+            return redirect("/")
+
     return render(request, 'PemoranchApp/registration/registration.html', {'form': form})
 
 
 def login_view(request):
-    return render(request, 'PemoranchApp/registration/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+            else:
+                form.add_error('password', 'Invalid username or password')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'PemoranchApp/registration/login.html', {'form': form})
 
 
 def logout_view(request):
